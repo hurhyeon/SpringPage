08523dc9aba22d816c84af7b2d290da255d75387
허현 <nsksjsk2@hanmail.net>
2024-02-03 11:20:17 +0900

    7

--- src/main/java/hello/core/disconut/RateDiscountPolicy.java
+++ src/main/java/hello/core/disconut/RateDiscountPolicy.java
@@ -1,10 +1,10 @@
 package hello.core.disconut;
 
+
+//test용도
 import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
