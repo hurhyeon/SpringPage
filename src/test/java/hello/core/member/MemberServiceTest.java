aa5dd19481e37bd6cda6e22520a5cda3fe75cda6
허현 <nsksjsk2@hanmail.net>
2024-02-06 01:15:06 +0900

    7

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+
+//test용
 public class MemberServiceTest {
     MemberService memberService;
 
