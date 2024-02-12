86c0fbd871b4755d9014bdb62daca3d306610e52
허현 <nsksjsk2@hanmail.net>
2024-02-13 01:43:47 +0900

    7.7

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+
+//test용도
 public class OrderServiceTest {
 
     MemberService memberService;
