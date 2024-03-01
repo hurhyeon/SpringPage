95dc3158817d9c9464529e02dc6072441d02e3de
허현 <nsksjsk2@hanmail.net>
2024-03-02 00:31:30 +0900

    final

--- src/main/java/hello/core/annotation/MainDiscountPolicy.java
+++ src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import java.lang.annotation.*;
 
 
-// test용도
+// test용
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
