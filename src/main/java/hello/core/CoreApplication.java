ec46a47f344b804a9897f94a1c5452c8b81eff43
허현 <nsksjsk2@hanmail.net>
2024-03-04 12:27:48 +0900

    realfinal

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//test
+//test용
 @SpringBootApplication
 public class CoreApplication {
 
