fd6cb8e9359815e3e516dbdbf853fcee621e2771
hurhyeon <90999300+hurhyeon@users.noreply.github.com>
2024-05-04 22:11:20 +0900

    Update AppConfig.java

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -20,13 +20,13 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
-        //1번
+        
         System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public OrderService orderService() {
-        //1번
+        
         System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
 
