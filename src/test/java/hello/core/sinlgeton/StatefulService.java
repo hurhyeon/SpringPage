90ab35619ddacfbb09a393558295455d41690cb2
hurhyeon <90999300+hurhyeon@users.noreply.github.com>
2024-06-06 20:47:39 +0900

    Update StatefulService.java

--- src/test/java/hello/core/sinlgeton/StatefulService.java
+++ src/test/java/hello/core/sinlgeton/StatefulService.java
@@ -2,7 +2,7 @@ package hello.core.sinlgeton;
 
 public class StatefulService {
 
-    //    private int price;  상태를 유지하는 필드
+//      private int price;  상태를 유지하는 필드
     public int order(String name, int price) {
         System.out.println("name = " + name + "price = " + price);
 //        this.price = price;
