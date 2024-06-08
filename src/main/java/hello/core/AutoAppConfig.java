fa471259f52027ebe176e2bb9c9fa4ecbc1f6891
hurhyeon <90999300+hurhyeon@users.noreply.github.com>
2024-06-08 18:27:09 +0900

    Update AutoAppConfig.java

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,11 +7,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import static org.springframework.context.annotation.ComponentScan.*;
+
 @Configuration
 @ComponentScan(
 //        basePackages = "hello.core",
 
-        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+
 public class AutoAppConfig {
 
 //    @Bean(name = "memoryMemberRepository")
