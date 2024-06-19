6d62ec5a36b22d199d67ca80b28cb565d8eb0da7
hurhyeon <90999300+hurhyeon@users.noreply.github.com>
2024-06-19 23:18:00 +0900

    Update BeanDefinitionTest.java

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -12,7 +12,7 @@ public class BeanDefinitionTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
-//GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
 
     @Test
     
