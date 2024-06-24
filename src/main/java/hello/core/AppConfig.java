477d4108260eca15df0d5ea9053dee06b382329f
hurhyeon <90999300+hurhyeon@users.noreply.github.com>
2024-06-24 17:57:33 +0900

    Update AppConfig.java

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -37,7 +37,7 @@ public class AppConfig {
 
     @Bean
     public DiscountPolicy discountPolicy(){
-//        return new FixDiscountPolicy();
+//      return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 }
