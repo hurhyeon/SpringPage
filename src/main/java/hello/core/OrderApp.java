65c67f66c72f34117b85f6578668d46b71f655d4
hurhyeon <90999300+hurhyeon@users.noreply.github.com>
2024-07-10 02:29:23 +0900

    Update OrderApp.java

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -14,9 +14,7 @@ public class OrderApp {
 
     public static void main(String[] args) {
 
-//        AppConfig appConfig = new AppConfig();
-//        MemberService memberService = appConfig.memberService();
-//        OrderService orderService = appConfig.orderService();
+
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
