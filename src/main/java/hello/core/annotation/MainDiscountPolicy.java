4c575b1802dda6837c496e83cdefc9d08c2e9543
hurhyeon <90999300+hurhyeon@users.noreply.github.com>
2024-07-12 22:41:58 +0900

    Update MainDiscountPolicy.java

--- src/main/java/hello/core/annotation/MainDiscountPolicy.java
+++ src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import java.lang.annotation.*;
 
 
-
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
